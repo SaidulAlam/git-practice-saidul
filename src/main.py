3cd04cbc1060b5a3f720339b7eec9045bd1c82fc
Sayed Saidul Alam <saidul@icddrb.net>
2026-05-01 21:15:41 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from datetime import date
+from utils import add, subtract
 
 print("Name: Sayed Saidul Alam")
 print("Today's date:", date.today())
+
+num_1 = 20
+num_2 = 5
+
+print("Addition:", add(num_1, num_2))
+print("Subtraction:", subtract(num_1, num_2))
